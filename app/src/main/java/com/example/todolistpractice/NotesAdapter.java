ba637c5efc8ca77d96131dee3287f398b89563d2
sargeliu <53758352+Sargeliu@users.noreply.github.com>
2023-07-13 20:37:11 +0500

    Добавление слушателей в адаптер

--- app/src/main/java/com/example/todolistpractice/NotesAdapter.java
+++ app/src/main/java/com/example/todolistpractice/NotesAdapter.java
@@ -14,6 +14,11 @@ import java.util.ArrayList;
 public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.NotesViewHolder> {
 
     private ArrayList<Note> notes = new ArrayList<>();
+    private OnNoteClickListener onNoteClickListener;
+
+    public void setOnNoteClickListener(OnNoteClickListener onNoteClickListener) {
+        this.onNoteClickListener = onNoteClickListener;
+    }
 
     public void setNotes(ArrayList<Note> notes) {
         this.notes = notes;
@@ -50,6 +55,15 @@ public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.NotesViewHol
         }
         int color = ContextCompat.getColor(viewHolder.itemView.getContext(), colorReId);
         viewHolder.textViewNote.setBackgroundColor(color);
+
+        viewHolder.textViewNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (onNoteClickListener != null) {
+                    onNoteClickListener.onNoteClick(note);
+                }
+            }
+        });
     }
 
     @Override
@@ -65,5 +79,10 @@ public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.NotesViewHol
             super(itemView);
             textViewNote = itemView.findViewById(R.id.textViewNote);
         }
+    }
+
+    interface OnNoteClickListener {
+
+        void onNoteClick(Note note);
     }
 }
